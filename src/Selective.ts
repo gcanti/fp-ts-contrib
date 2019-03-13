cf32d3c77e2167b23eb9bec449ba48ba61446280
Giovanni Gonzaga <giovanni@buildo.io>
2019-03-13 14:39:11 +0100

    add Selective typeclass

---
+++ src/Selective.ts
@@ -0,0 +1,270 @@
+/**
+ * @file The `Selective` type class is an abstraction between applicative functors and monads.
+ *
+ * Ported from https://github.com/snowleopard/selective
+ *
+ * In addition to the `Applicative` laws, instances of the `Selective` type class must satisfy
+ * a few laws to make it possible to refactor selective computations:
+ *
+ * 1. Identity: `x <*? pure id = either id id <$> x`
+ * 2. Distributivity: `pure x <*? (y *> z) = (pure x <*? y) *> (pure x <*? z)`
+ * 3. Associativity: `x <*? (y <*? z) = (f <$> x) <*? (g <$> y) <*? (h <$> z)`
+ *
+ * Note. For `Selective` instances that are also `Monad`s, `select` must be equal to the one derived on `chain`
+ *
+ */
+import {
+  Applicative,
+  Applicative1,
+  Applicative2,
+  Applicative2C,
+  Applicative3,
+  Applicative3C
+} from 'fp-ts/lib/Applicative'
+import { URIS, URIS2, URIS3, HKT, Type, Type2, Type3 } from 'fp-ts/lib/HKT'
+import { Either, left, right } from 'fp-ts/lib/Either'
+import { constant, Lazy, identity } from 'fp-ts/lib/function'
+import { Setoid } from 'fp-ts/lib/Setoid'
+import { array } from 'fp-ts/lib/Array'
+import { Semigroup } from 'fp-ts/lib/Semigroup'
+import { URI, Validation, failure, success, getApplicative } from 'fp-ts/lib/Validation'
+import { Option, none, some, option as _option } from 'fp-ts/lib/Option'
+import { Task, task as _task } from 'fp-ts/lib/Task'
+
+/**
+ * @since 1.15.0
+ */
+export interface Selective<F> extends Applicative<F> {
+  readonly select: <A, B>(fa: HKT<F, Either<A, B>>, f: HKT<F, (a: A) => B>) => HKT<F, B>
+}
+
+export interface Selective1<F extends URIS> extends Applicative1<F> {
+  readonly select: <A, B>(fa: Type<F, Either<A, B>>, f: Type<F, (a: A) => B>) => Type<F, B>
+}
+
+export interface Selective2<M extends URIS2> extends Applicative2<M> {
+  readonly select: <L, A, B>(fa: Type2<M, L, Either<A, B>>, f: Type2<M, L, (a: A) => B>) => Type2<M, L, B>
+}
+
+export interface Selective3<M extends URIS3> extends Applicative3<M> {
+  readonly select: <U, L, A, B>(fa: Type3<M, U, L, Either<A, B>>, f: Type3<M, U, L, (a: A) => B>) => Type3<M, U, L, B>
+}
+
+export interface Selective2C<M extends URIS2, L> extends Applicative2C<M, L> {
+  readonly select: <A, B>(fa: Type2<M, L, Either<A, B>>, f: Type2<M, L, (a: A) => B>) => Type2<M, L, B>
+}
+
+export interface Selective3C<M extends URIS3, U, L> extends Applicative3C<M, U, L> {
+  readonly select: <U, A, B>(fa: Type3<M, U, L, Either<A, B>>, f: Type3<M, U, L, (a: A) => B>) => Type3<M, U, L, B>
+}
+
+// branch :: Selective f => f (Either a b) -> f (a -> c) -> f (b -> c) -> f c
+export function branch<F extends URIS3>(
+  S: Selective3<F>
+): <U, L, A, B, C>(
+  x: Type3<F, U, L, Either<A, B>>,
+  l: Type3<F, U, L, (a: A) => C>,
+  r: Type3<F, U, L, (b: B) => C>
+) => Type3<F, U, L, A>
+export function branch<F extends URIS2>(
+  S: Selective2<F>
+): <L, A, B, C>(
+  x: Type2<F, L, Either<A, B>>,
+  l: Type2<F, L, (a: A) => C>,
+  r: Type2<F, L, (b: B) => C>
+) => Type2<F, L, A>
+export function branch<F extends URIS>(
+  S: Selective1<F>
+): <A, B, C>(x: Type<F, Either<A, B>>, l: Type<F, (a: A) => C>, r: Type<F, (b: B) => C>) => Type<F, C>
+export function branch<F extends URIS>(
+  S: Selective<F>
+): <A, B, C>(x: HKT<F, Either<A, B>>, l: HKT<F, (a: A) => C>, r: HKT<F, (b: B) => C>) => HKT<F, C>
+export function branch<F extends URIS>(S: Selective<F>) {
+  return <A, B, C>(x: HKT<F, Either<A, B>>, l: HKT<F, (a: A) => C>, r: HKT<F, (b: B) => C>): HKT<F, C> => {
+    // branch x l r = fmap (fmap Left) x <*? fmap (fmap Right) l <*? r
+    const xx: HKT<F, Either<A, Either<B, C>>> = S.map(x, x =>
+      x.fold(a => left<A, Either<B, C>>(a), b => right<A, Either<B, C>>(left<B, C>(b)))
+    )
+    const ll: HKT<F, (a: A) => Either<B, C>> = S.map(l, l => (a: A) => right<B, C>(l(a)))
+    return S.select(S.select(xx, ll), r)
+  }
+}
+
+const leftUnit = left<void, void>(undefined)
+const rightUnit = right<void, void>(undefined)
+
+// ifS :: Selective f => f Bool -> f a -> f a -> f a
+export function ifS<F extends URIS3>(
+  S: Selective3<F>
+): <U, L, A>(x: Type3<F, U, L, boolean>, t: Type3<F, U, L, A>, e: Type3<F, U, L, A>) => Type3<F, U, L, A>
+export function ifS<F extends URIS2>(
+  S: Selective2<F>
+): <L, A>(x: Type2<F, L, boolean>, t: Type2<F, L, A>, e: Type2<F, L, A>) => Type2<F, L, A>
+export function ifS<F extends URIS2, L>(
+  S: Selective2C<F, L>
+): <L, A>(x: Type2<F, L, boolean>, t: Type2<F, L, A>, e: Type2<F, L, A>) => Type2<F, L, A>
+export function ifS<F extends URIS>(
+  S: Selective1<F>
+): <A>(x: Type<F, boolean>, t: Type<F, A>, e: Type<F, A>) => Type<F, A>
+export function ifS<F extends URIS>(S: Selective<F>): <A>(x: HKT<F, boolean>, t: HKT<F, A>, e: HKT<F, A>) => HKT<F, A>
+export function ifS<F extends URIS>(S: Selective<F>) {
+  const branchF = branch(S)
+  return <A>(x: HKT<F, boolean>, t: HKT<F, A>, e: HKT<F, A>): HKT<F, A> => {
+    // ifS x t e = branch (bool (Right ()) (Left ()) <$> x) (const <$> t) (const <$> e)
+    return branchF(S.map(x, x => (x ? leftUnit : rightUnit)), S.map(t, constant), S.map(e, constant))
+  }
+}
+
+function eliminateMatch<A>(Sa: Setoid<A>) {
+  return (a: A) => <B>(b: Either<A, B>): Either<void, Either<A, B>> =>
+    b.fold(
+      y => (Sa.equals(a, y) ? left<void, Either<A, B>>(undefined) : right<void, Either<A, B>>(left(y))),
+      y => right<void, Either<A, B>>(right<A, B>(y))
+    )
+}
+
+// eliminate :: (Eq a, Selective f) => a -> f b -> f (Either a b) -> f (Either a b)
+export function eliminate<F extends URIS3, A>(
+  S: Selective3<F>,
+  Sa: Setoid<A>
+): <U, L, B>(x: A, fb: Type3<F, U, L, B>, fa: Type3<F, U, L, Either<A, B>>) => Type3<F, U, L, Either<A, B>>
+export function eliminate<F extends URIS2, A>(
+  S: Selective2<F>,
+  Sa: Setoid<A>
+): <L, B>(x: A, fb: Type2<F, L, B>, fa: Type2<F, L, Either<A, B>>) => Type2<F, L, Either<A, B>>
+export function eliminate<F extends URIS, A>(
+  S: Selective1<F>,
+  Sa: Setoid<A>
+): <B>(x: A, fb: Type<F, B>, fa: Type<F, Either<A, B>>) => Type<F, Either<A, B>>
+export function eliminate<F extends URIS, A>(
+  S: Selective<F>,
+  Sa: Setoid<A>
+): <B>(x: A, fb: HKT<F, B>, fa: HKT<F, Either<A, B>>) => HKT<F, Either<A, B>>
+export function eliminate<F extends URIS, A>(S: Selective<F>, Sa: Setoid<A>) {
+  const match = eliminateMatch(Sa)
+  return <B>(x: A, fb: HKT<F, B>, fa: HKT<F, Either<A, B>>): HKT<F, Either<A, B>> => {
+    // eliminate x fb fa = select (match x <$> fa) (const . Right <$> fb)
+    //   where
+    //     match _ (Right y) = Right (Right y)
+    //     match x (Left  y) = if x == y then Left () else Right (Left y)
+    //
+    return S.select(S.map(fa, match(x)), S.map(fb, b => constant(right<A, B>(b))))
+  }
+}
+
+// whenS :: Selective f => f Bool -> f () -> f ()
+export function when<F extends URIS3>(
+  S: Selective3<F>
+): <U, L>(x: Type3<F, U, L, boolean>, y: Type3<F, U, L, Lazy<void>>) => Type3<F, U, L, void>
+export function when<F extends URIS2>(
+  S: Selective2<F>
+): <L>(x: Type2<F, L, boolean>, y: Type2<F, L, Lazy<void>>) => Type2<F, L, void>
+export function when<F extends URIS>(S: Selective1<F>): (x: Type<F, boolean>, y: Type<F, Lazy<void>>) => Type<F, void>
+export function when<F extends URIS>(S: Selective<F>): (x: HKT<F, boolean>, y: HKT<F, Lazy<void>>) => HKT<F, void>
+export function when<F extends URIS>(S: Selective<F>) {
+  return (x: HKT<F, boolean>, y: HKT<F, Lazy<void>>): HKT<F, void> => {
+    // whenS x y = select (bool (Right ()) (Left ()) <$> x) (const <$> y)
+    return S.select(S.map(x, x => (x ? leftUnit : rightUnit)), y)
+  }
+}
+
+// (<||>) :: Selective f => f Bool -> f Bool -> f Bool
+export function or<F extends URIS3>(
+  S: Selective3<F>
+): <U, L>(x: Type3<F, U, L, boolean>, y: Type3<F, U, L, boolean>) => Type3<F, U, L, boolean>
+export function or<F extends URIS2>(
+  S: Selective2<F>
+): <L>(x: Type2<F, L, boolean>, y: Type2<F, L, boolean>) => Type2<F, L, boolean>
+export function or<F extends URIS>(S: Selective1<F>): (x: Type<F, boolean>, y: Type<F, boolean>) => Type<F, boolean>
+export function or<F extends URIS>(S: Selective<F>): (x: HKT<F, boolean>, y: HKT<F, boolean>) => HKT<F, boolean>
+export function or<F extends URIS>(S: Selective<F>) {
+  const orIf = ifS(S)
+  return (x: HKT<F, boolean>, y: HKT<F, boolean>): HKT<F, boolean> => {
+    // x <||> y = ifS x (pure True) y
+    return orIf(x, S.of(true), y)
+  }
+}
+
+// (<&&>) :: Selective f => f Bool -> f Bool -> f Bool
+export function and<F extends URIS3>(
+  S: Selective3<F>
+): <U, L>(x: Type3<F, U, L, boolean>, y: Type3<F, U, L, boolean>) => Type3<F, U, L, boolean>
+export function and<F extends URIS2>(
+  S: Selective2<F>
+): <L>(x: Type2<F, L, boolean>, y: Type2<F, L, boolean>) => Type2<F, L, boolean>
+export function and<F extends URIS>(S: Selective1<F>): (x: Type<F, boolean>, y: Type<F, boolean>) => Type<F, boolean>
+export function and<F extends URIS>(S: Selective<F>): (x: HKT<F, boolean>, y: HKT<F, boolean>) => HKT<F, boolean>
+export function and<F extends URIS>(S: Selective<F>) {
+  const ifS_ = ifS(S)
+  return (x: HKT<F, boolean>, y: HKT<F, boolean>): HKT<F, boolean> => {
+    // x <&&> y = ifS x y (pure False)
+    return ifS_(x, y, S.of(false))
+  }
+}
+
+// anyS :: Selective f => (a -> f Bool) -> [a] -> f Bool
+export function any<F extends URIS3>(
+  S: Selective3<F>
+): <U, L, A>(as: Array<A>, p: (a: A) => Type3<F, U, L, boolean>) => Type3<F, U, L, boolean>
+export function any<F extends URIS2>(
+  S: Selective2<F>
+): <L, A>(as: Array<A>, p: (a: A) => Type2<F, L, boolean>) => Type2<F, L, boolean>
+export function any<F extends URIS>(
+  S: Selective1<F>
+): <A>(as: Array<A>, p: (a: A) => Type<F, boolean>) => Type<F, boolean>
+export function any<F extends URIS>(S: Selective<F>): <A>(as: Array<A>, p: (a: A) => HKT<F, boolean>) => HKT<F, boolean>
+export function any<F extends URIS>(S: Selective<F>) {
+  const orS = or(S)
+  return <A>(as: Array<A>, p: (a: A) => HKT<F, boolean>): HKT<F, boolean> => {
+    // anyS p = foldr ((<||>) . p) (pure False)
+    return array.foldr(as, S.of(false), (a, b) => orS(b, p(a)))
+  }
+}
+
+// allS :: Selective f => (a -> f Bool) -> [a] -> f Bool
+export function all<F extends URIS3>(
+  S: Selective3<F>
+): <U, L, A>(as: Array<A>, p: (a: A) => Type3<F, U, L, boolean>) => Type3<F, U, L, boolean>
+export function all<F extends URIS2>(
+  S: Selective2<F>
+): <L, A>(as: Array<A>, p: (a: A) => Type2<F, L, boolean>) => Type2<F, L, boolean>
+export function all<F extends URIS>(
+  S: Selective1<F>
+): <A>(as: Array<A>, p: (a: A) => Type<F, boolean>) => Type<F, boolean>
+export function all<F extends URIS>(S: Selective<F>): <A>(as: Array<A>, p: (a: A) => HKT<F, boolean>) => HKT<F, boolean>
+export function all<F extends URIS>(S: Selective<F>) {
+  const andS = and(S)
+  return <A>(as: Array<A>, p: (a: A) => HKT<F, boolean>): HKT<F, boolean> => {
+    // allS p = foldr ((<&&>) . p) (pure True)
+    return array.foldr(as, S.of(true), (a, b) => andS(b, p(a)))
+  }
+}
+
+export const getValidationSelective = <L>(S: Semigroup<L>): Selective2C<URI, L> => {
+  // instance Semigroup e => Selective (Validation e) where
+  //   select (Success (Right b)) _ = Success b
+  //   select (Success (Left  a)) f = ($a) <$> f
+  //   select (Failure e        ) _ = Failure e
+  const select: <A, B>(fa: Validation<L, Either<A, B>>, f: Validation<L, (a: A) => B>) => Validation<L, B> = (fa, f) =>
+    fa.fold(e => failure(e), e => e.fold(a => f.map(ab => ab(a)), b => success(b)))
+  return {
+    ...getApplicative(S),
+    select
+  }
+}
+
+export const selectOption: <A, B>(fa: Option<Either<A, B>>, fab: Option<(a: A) => B>) => Option<B> = (fa, fab) =>
+  fa.fold(none, e => e.fold(a => fab.map(f => f(a)), b => some(b)))
+
+export const option: typeof _option & Selective1<typeof _option.URI> = {
+  ..._option,
+  select: selectOption
+}
+
+export const selectTask: <A, B>(fa: Task<Either<A, B>>, fab: Task<(a: A) => B>) => Task<B> = (fa, fab) =>
+  fa.chain(e => fab.map(f => e.fold(f, identity)))
+
+export const task: typeof _task & Selective1<typeof _task.URI> = {
+  ..._task,
+  select: selectTask
+}
---
+++ test/Selective.ts
@@ -0,0 +1,157 @@
+import * as assert from 'assert'
+import { constant, Function1, tuple } from 'fp-ts/lib/function'
+import {
+  ifS,
+  when,
+  branch,
+  or,
+  and,
+  any,
+  all,
+  Selective2C,
+  getValidationSelective,
+  option,
+  task
+} from '../src/Selective'
+import { some } from 'fp-ts/lib/Option'
+import { left, right } from 'fp-ts/lib/Either'
+import { URIS2, Type2 } from 'fp-ts/lib/HKT'
+import { success, failure } from 'fp-ts/lib/Validation'
+import { liftA2 } from 'fp-ts/lib/Apply'
+import { getArrayMonoid } from 'fp-ts/lib/Monoid'
+
+describe('Selective', () => {
+  it('branch', () => {
+    const add: Function1<number, number> = a => a + a
+    const length: Function1<string, number> = a => a.length
+    const addSpy = jest.fn(add)
+    const lengthSpy = jest.fn(length)
+    assert.deepStrictEqual(branch(option)(some(left<string, number>('foo')), some(lengthSpy), some(addSpy)), some(3))
+    assert.deepStrictEqual(addSpy.mock.calls.length, 0)
+    assert.deepStrictEqual(lengthSpy.mock.calls.length, 1)
+    assert.deepStrictEqual(branch(option)(some(right<string, number>(2)), some(lengthSpy), some(addSpy)), some(4))
+    assert.deepStrictEqual(addSpy.mock.calls.length, 1)
+    assert.deepStrictEqual(lengthSpy.mock.calls.length, 1)
+  })
+
+  it('ifS', () => {
+    const one = constant(1)
+    const two = constant(2)
+    const oneSpy = jest.fn(one)
+    const twoSpy = jest.fn(two)
+    assert.deepStrictEqual(ifS(option)(some(true), some(oneSpy), some(twoSpy)).map(f => f()), some(one).map(f => f()))
+    assert.deepStrictEqual(oneSpy.mock.calls.length, 1)
+    assert.deepStrictEqual(twoSpy.mock.calls.length, 0)
+    assert.deepStrictEqual(ifS(option)(some(false), some(oneSpy), some(twoSpy)).map(f => f()), some(two).map(f => f()))
+    assert.deepStrictEqual(oneSpy.mock.calls.length, 1)
+    assert.deepStrictEqual(twoSpy.mock.calls.length, 1)
+  })
+
+  it('when', () => {
+    const e = jest.fn()
+    assert.deepStrictEqual(when(option)(some(false), some(e)), some(undefined))
+    assert.deepStrictEqual(e.mock.calls.length, 0)
+    assert.deepStrictEqual(when(option)(some(true), some(e)), some(undefined))
+    assert.deepStrictEqual(e.mock.calls.length, 1)
+
+    return Promise.all([when(task)(task.of(false), task.of(e)).run(), task.of(undefined).run()])
+      .then(([r1, r2]) => {
+        assert.deepStrictEqual(r1, r2)
+        assert.deepStrictEqual(e.mock.calls.length, 1)
+      })
+      .then(() => Promise.all([when(task)(task.of(true), task.of(e)).run(), task.of(undefined).run()]))
+      .then(([r1, r2]) => {
+        assert.deepStrictEqual(r1, r2)
+        assert.deepStrictEqual(e.mock.calls.length, 2)
+      })
+  })
+
+  it('or', () => {
+    assert.deepStrictEqual(or(option)(some(false), some(false)), some(false))
+    assert.deepStrictEqual(or(option)(some(true), some(false)), some(true))
+    assert.deepStrictEqual(or(option)(some(false), some(true)), some(true))
+    assert.deepStrictEqual(or(option)(some(true), some(true)), some(true))
+  })
+
+  it('and', () => {
+    assert.deepStrictEqual(and(option)(some(false), some(false)), some(false))
+    assert.deepStrictEqual(and(option)(some(true), some(false)), some(false))
+    assert.deepStrictEqual(and(option)(some(false), some(true)), some(false))
+    assert.deepStrictEqual(and(option)(some(true), some(true)), some(true))
+  })
+
+  it('any', () => {
+    assert.deepStrictEqual(any(option)([false, false], option.of), some(false))
+    assert.deepStrictEqual(any(option)([false, true], option.of), some(true))
+    assert.deepStrictEqual(any(option)([true, true], option.of), some(true))
+  })
+
+  it('all', () => {
+    assert.deepStrictEqual(all(option)([false, false], option.of), some(false))
+    assert.deepStrictEqual(all(option)([false, true], option.of), some(false))
+    assert.deepStrictEqual(all(option)([true, true], option.of), some(true))
+  })
+
+  it('Validation', () => {
+    // type Radius = Word; type Width = Word; type Height = Word
+    // data Shape = Circle Radius | Rectangle Width Height
+    // shape :: Selective f => f Bool -> f Radius -> f Width -> f Height -> f Shape
+    // shape x r w h = ifS x (Circle <$> r) (Rectangle <$> w <*> h)
+    type Radius = number
+    type Width = number
+    type Height = number
+    type Shape = { type: 'Circle'; radius: Radius } | { type: 'Square'; width: Width; height: Height }
+    const circle = (radius: Radius): Shape => ({ type: 'Circle', radius })
+    const square = (width: Width): ((height: Height) => Shape) => height => ({ type: 'Square', width, height })
+    function shape<F extends URIS2>(S: Selective2C<F, Array<string>>) {
+      const ifS_ = ifS(S)
+      return (
+        x: Type2<F, Array<string>, boolean>,
+        r: Type2<F, Array<string>, Radius>,
+        w: Type2<F, Array<string>, Width>,
+        h: Type2<F, Array<string>, Height>
+      ): Type2<F, Array<string>, Shape> => ifS_(x, S.map(r, circle), liftA2(S)(square)(w)(h))
+    }
+
+    // λ> shape (Success True) (Success 1) (Failure ["width?"]) (Failure ["height?"])
+    // Success (Circle 1)
+    // λ> shape (Success False) (Failure ["radius?"]) (Success 2) (Success 3)
+    // Success (Rectangle 2 3)
+    // λ> shape (Success False) (Success 1) (Failure ["width?"]) (Failure ["height?"])
+    // Failure ["width?", "height?"]
+    // λ> shape (Failure ["choice?"]) (Failure ["radius?"]) (Success 2) (Failure ["height?"])
+    // Failure ["choice?"]
+    const validationS = getValidationSelective(getArrayMonoid<string>())
+    const vShape = shape(validationS)
+    assert.deepStrictEqual(
+      vShape(success(true), success(1), failure(['width?']), failure(['height?'])),
+      success(circle(1))
+    )
+    assert.deepStrictEqual(vShape(success(false), failure(['radius?']), success(2), success(3)), success(square(2)(3)))
+    assert.deepStrictEqual(
+      vShape(success(false), success(1), failure(['width?']), failure(['height?'])),
+      failure(['width?', 'height?'])
+    )
+    assert.deepStrictEqual(
+      vShape(failure(['choice?']), failure(['radius?']), success(2), failure(['height?'])),
+      failure(['choice?'])
+    )
+
+    // twoShapes :: Selective f => f Shape -> f Shape -> f (Shape, Shape)
+    // twoShapes s1 s2 = (,) <$> s1 <*> s2
+    function twoShapes<F extends URIS2>(S: Selective2C<F, Array<string>>) {
+      return (
+        s1: Type2<F, Array<string>, Shape>,
+        s2: Type2<F, Array<string>, Shape>
+      ): Type2<F, Array<string>, [Shape, Shape]> => S.ap(S.map(s1, (s1: Shape) => (s2: Shape) => tuple(s1, s2)), s2)
+    }
+
+    // λ> s1 = shape (Failure ["choice 1?"]) (Success 1) (Failure ["width 1?"]) (Success 3)
+    // λ> s2 = shape (Success False) (Success 1) (Success 2) (Failure ["height 2?"])
+    // λ> twoShapes s1 s2
+    // Failure ["choice 1?","height 2?"]
+    const s1 = vShape(failure(['choice 1?']), success(1), failure(['width 1?']), success(3))
+    const s2 = vShape(success(false), success(1), success(2), failure(['height 2?']))
+    assert.deepStrictEqual(twoShapes(validationS)(s1, s2), failure(['choice 1?', 'height 2?']))
+  })
+})
