e0ac8209ad8ea18e62519709557dd9ac828240a7
Paul Gray <pfbgray@gmail.com>
2019-03-21 09:35:21 -0400

    Adding Do for composing chainables

--- dtslint/ts3.0/index.ts
+++ dtslint/ts3.0/index.ts
@@ -3,6 +3,8 @@ import * as IO from 'fp-ts/lib/IO'
 import * as Task from 'fp-ts/lib/Task'
 import * as TaskEither from 'fp-ts/lib/TaskEither'
 import * as ReaderTaskEither from 'fp-ts/lib/ReaderTaskEither'
+import { either, right } from 'fp-ts/lib/Either'
+import { Do } from '../../src/Do'
 
 //
 // time
@@ -12,3 +14,7 @@ const time1 = time(IO.io) // $ExpectType <A>(ma: IO<A>) => IO<[A, number]>
 const time2 = time(Task.task) // $ExpectType <A>(ma: Task<A>) => Task<[A, number]>
 const time3 = time(TaskEither.taskEither) // $ExpectType <L, A>(ma: TaskEither<L, A>) => TaskEither<L, [A, number]>
 const time4 = time(ReaderTaskEither.readerTaskEither) // $ExpectType <U, L, A>(ma: ReaderTaskEither<U, L, A>) => ReaderTaskEither<U, L, [A, number]>
+
+Do(either)
+  .bind('name', right<string, string>('bob'))
+  .bindL('bad', () => right<boolean, number>(54)) // $ExpectError
