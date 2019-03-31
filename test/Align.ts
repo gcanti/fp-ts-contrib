88cc8849def79cbb41db2bc1193601d969ae347d
Mathias Andersson <wraul@dbox.se>
2019-03-31 12:12:24 +0200

    Implement Align for Record

--- test/Align.ts
+++ test/Align.ts
@@ -6,6 +6,7 @@ import { identity } from 'fp-ts/lib/function'
 import { salign, padZip, padZipWith } from '../src/Align'
 import { alignArray, lpadZipWith, lpadZip, rpadZipWith, rpadZip } from '../src/Align/Array'
 import { alignOption } from '../src/Align/Option'
+import * as alignRecord from '../src/Align/Record'
 
 describe('Align', () => {
   it('salign', () => {
@@ -95,6 +96,30 @@ describe('Align', () => {
       assert.deepStrictEqual(alignOption.alignWith(some(1), alignOption.nil<string>(), f), some('1'))
       assert.deepStrictEqual(alignOption.alignWith(alignOption.nil<number>(), some('a'), f), some('a'))
       assert.deepStrictEqual(alignOption.alignWith(alignOption.nil<number>(), alignOption.nil<string>(), f), none)
+    })
+  })
+
+  describe('Record', () => {
+    it('align', () => {
+      assert.deepStrictEqual(alignRecord.align({ a: 1, b: 2 }, { a: 'a', b: 'b' }), {
+        a: both(1, 'a'),
+        b: both(2, 'b')
+      })
+      assert.deepStrictEqual(alignRecord.align({ a: 1, b: 2 }, { a: 'a' }), { a: both(1, 'a'), b: this_(2) })
+      assert.deepStrictEqual(alignRecord.align({ a: 1 }, { a: 'a', b: 'b' }), { a: both(1, 'a'), b: that('b') })
+      assert.deepStrictEqual(alignRecord.align({}, {}), {})
+      assert.deepStrictEqual(alignRecord.align({ a: 1 }, alignRecord.nil<string>()), { a: this_(1) })
+      assert.deepStrictEqual(alignRecord.align(alignRecord.nil<number>(), { a: 'a' }), { a: that('a') })
+    })
+
+    it('alignWith', () => {
+      const f = (x: These<number, string>) => x.fold(a => a.toString(), identity, (a, b) => b + a)
+      assert.deepStrictEqual(alignRecord.alignWith({ a: 1, b: 2 }, { a: 'a', b: 'b' }, f), { a: 'a1', b: 'b2' })
+      assert.deepStrictEqual(alignRecord.alignWith({ a: 1, b: 2 }, { a: 'a' }, f), { a: 'a1', b: '2' })
+      assert.deepStrictEqual(alignRecord.alignWith({ a: 1 }, { a: 'a', b: 'b' }, f), { a: 'a1', b: 'b' })
+      assert.deepStrictEqual(alignRecord.alignWith({}, {}, f), {})
+      assert.deepStrictEqual(alignRecord.alignWith({ a: 1 }, alignRecord.nil<string>(), f), { a: '1' })
+      assert.deepStrictEqual(alignRecord.alignWith(alignRecord.nil<number>(), { a: 'a' }, f), { a: 'a' })
     })
   })
 })
