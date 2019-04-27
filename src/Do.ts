864d3ac31d6807e493b102d0d6e583acd6f5724a
gcanti <giulio.canti@gmail.com>
2019-04-27 15:00:39 +0200

    Improve Do performance

--- src/Do.ts
+++ src/Do.ts
@@ -73,6 +73,90 @@ export interface Do0<M, S extends object> {
   done: () => HKT<M, S>
 }
 
+type LinkedList<A> = { type: 'Nil'; length: number } | { type: 'Cons'; head: A; tail: LinkedList<A>; length: number }
+
+const nil: LinkedList<never> = { type: 'Nil', length: 0 }
+
+const cons = <A>(head: A, tail: LinkedList<A>): LinkedList<A> => ({
+  type: 'Cons',
+  head,
+  tail,
+  length: tail.length + 1
+})
+
+const toArray = <A>(list: LinkedList<A>): Array<A> => {
+  const len = list.length
+  const r: Array<A> = new Array(len)
+  let l: LinkedList<A> = list
+  let i = 1
+  while (l.type !== 'Nil') {
+    r[len - i] = l.head
+    i++
+    l = l.tail
+  }
+  return r
+}
+
+type Action<M> =
+  | { type: 'do'; action: HKT<M, unknown> }
+  | { type: 'doL'; f: (s: unknown) => HKT<M, unknown> }
+  | { type: 'bind'; name: string; action: HKT<M, unknown> }
+  | { type: 'bindL'; name: string; f: (s: unknown) => HKT<M, unknown> }
+
+class DoClass<M> {
+  constructor(readonly M: Monad<M>, private actions: LinkedList<Action<M>>) {}
+  do(action: HKT<M, unknown>): DoClass<M> {
+    return new DoClass(this.M, cons({ type: 'do', action }, this.actions))
+  }
+  doL(f: (s: unknown) => HKT<M, unknown>): DoClass<M> {
+    return new DoClass(this.M, cons({ type: 'doL', f }, this.actions))
+  }
+  bind(name: string, action: HKT<M, unknown>): DoClass<M> {
+    return new DoClass(this.M, cons({ type: 'bind', name, action }, this.actions))
+  }
+  bindL(name: string, f: (s: unknown) => HKT<M, unknown>): DoClass<M> {
+    return new DoClass(this.M, cons({ type: 'bindL', name, f }, this.actions))
+  }
+  return<B>(f: (s: unknown) => B): HKT<M, B> {
+    return this.M.map(this.done(), f)
+  }
+  done(): HKT<M, unknown> {
+    const actions = this.actions
+    const len = actions.length
+    const as = toArray(actions)
+    let result: HKT<M, any> = this.M.of({})
+    const M = this.M
+    for (let i = 0; i < len; i++) {
+      const a = as[i]
+      switch (a.type) {
+        case 'do':
+          result = M.chain(result, s => M.map(a.action, () => s))
+          break
+        case 'doL':
+          result = M.chain(result, s => M.map(a.f(s), () => s))
+          break
+        case 'bind':
+          result = M.chain(result, s =>
+            M.map(a.action, b => {
+              s[a.name] = b
+              return s
+            })
+          )
+          break
+        case 'bindL':
+          result = M.chain(result, s =>
+            M.map(a.f(s), b => {
+              s[a.name] = b
+              return s
+            })
+          )
+          break
+      }
+    }
+    return result
+  }
+}
+
 /**
  * This function provides a simuation of Haskell do notation. The `bind` / `bindL` functions contributes to a threaded
  * scope that is available to each subsequent step. The `do` / `doL` functions can be used to perform computations that
@@ -100,28 +184,5 @@ export function Do<M extends URIS2, L>(M: Monad2C<M, L>): Do2C<M, {}, L>
 export function Do<M extends URIS>(M: Monad1<M>): Do1<M, {}>
 export function Do<M>(M: Monad<M>): Do0<M, {}>
 export function Do<M>(M: Monad<M>): Do0<M, {}> {
-  function toDo<S extends object>(ms: HKT<M, S>): Do0<M, S> {
-    return {
-      do(mv: HKT<M, unknown>): Do0<M, S> {
-        return toDo(M.chain(ms, a => M.map(mv, () => a)))
-      },
-      doL(fmv: (s: S) => HKT<M, unknown>): Do0<M, S> {
-        return toDo(M.chain(ms, a => M.map(fmv(a), () => a)))
-      },
-      bind<N extends string, B>(name: Exclude<N, keyof S>, mb: HKT<M, B>): Do0<M, S & { [K in N]: B }> {
-        return toDo(M.chain(ms, a => M.map(mb, b => ({ ...(a as object), [name]: b })))) as any
-      },
-      bindL<N extends string, B>(name: Exclude<N, keyof S>, fmb: (s: S) => HKT<M, B>): Do0<M, S & { [K in N]: B }> {
-        return toDo(M.chain(ms, a => M.map(fmb(a), b => ({ ...(a as object), [name]: b })))) as any
-      },
-      return<B>(f: (s: S) => B): HKT<M, B> {
-        return M.map(ms, f)
-      },
-      done(): HKT<M, S> {
-        return ms
-      }
-    }
-  }
-
-  return toDo(M.of({}))
+  return new DoClass(M, nil) as any
 }
