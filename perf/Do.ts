55fe295e5d984a6b54c72005277733fd893f2234
gcanti <giulio.canti@gmail.com>
2019-04-27 15:00:39 +0200

    Improve Do performance

---
+++ perf/Do.ts
@@ -0,0 +1,38 @@
+import * as Benchmark from 'benchmark'
+import { Do } from '../src/Do'
+import { option, some } from 'fp-ts/lib/Option'
+
+const suite = new Benchmark.Suite()
+
+// const chain = option.chain
+
+// // tslint:disable-next-line: no-console
+// console.log(
+//   Do(option)
+//     .bind('a', some('a'))
+//     .bind('b', some('b'))
+//     .bind('c', some('c'))
+//     .bind('d', some('d'))
+//     .bind('e', some('e'))
+//     .done()
+// )
+
+suite
+  .add('Do', function() {
+    Do(option)
+      .bind('a', some('a'))
+      .bind('b', some('b'))
+      .bind('c', some('c'))
+      .bind('d', some('d'))
+      .bind('e', some('e'))
+      .done()
+  })
+  .on('cycle', function(event: any) {
+    // tslint:disable-next-line: no-console
+    console.log(String(event.target))
+  })
+  .on('complete', function(this: any) {
+    // tslint:disable-next-line: no-console
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+  })
+  .run({ async: true })
