3faea40e41f3ef286a9e0ae687a3a48401e25b28
Gabriele Petronella <gabriele@buildo.io>
2019-05-04 14:49:47 +0200

    Add all tests for TaskOption and TaskValidation

--- test/TaskOption.ts
+++ test/TaskOption.ts
@@ -1,8 +1,70 @@
-import { TaskOption } from '../src/TaskOption'
-import { delay } from 'fp-ts/lib/Task'
-import { some } from 'fp-ts/lib/Option'
+import { TaskOption, taskOption, none as taskOptionNone, fromOption, fromTask, tryCatch } from '../src/TaskOption'
+import { delay, Task } from 'fp-ts/lib/Task'
+import { some, none } from 'fp-ts/lib/Option'
 
 describe('TaskOption', () => {
+  it('map', () => {
+    const double = (n: number): number => n * 2
+    return taskOption
+      .map(taskOption.of(1), double)
+      .run()
+      .then(e => expect(e).toEqual(some(2)))
+  })
+
+  it('ap', () => {
+    const double = (n: number): number => n * 2
+    const fab = taskOption.of(double)
+    const fa = taskOption.of(1)
+    return Promise.all([fa.ap(fab).run(), fab.ap_(fa).run(), taskOption.ap(fab, fa).run()]).then(([e1, e2, e3]) => {
+      expect(e1).toEqual(some(2))
+      expect(e1).toEqual(e2)
+      expect(e1).toEqual(e3)
+    })
+  })
+
+  it('chain', () => {
+    const te1 = taskOption.chain(taskOption.of('foo'), a => (a.length > 2 ? taskOption.of(a.length) : taskOptionNone))
+    const te2 = taskOption.chain(taskOption.of<string>('a'), a =>
+      a.length > 2 ? taskOption.of(a.length) : taskOptionNone
+    )
+    return Promise.all([te1.run(), te2.run()]).then(([e1, e2]) => {
+      expect(e1).toEqual(some(3))
+      expect(e2).toEqual(none)
+    })
+  })
+
+  it('fold', () => {
+    const g = (n: number): boolean => n > 2
+    const te1 = taskOption.of<number>(1).fold(false, g)
+    const te2 = taskOptionNone.fold(true, g)
+    return Promise.all([te1.run(), te2.run()]).then(([b1, b2]) => {
+      expect(b1).toEqual(false)
+      expect(b2).toEqual(true)
+    })
+  })
+
+  it('getOrElse', () => {
+    const v: number = 42
+    const te1 = taskOption.of<number>(1).getOrElse(v)
+    const te2 = fromOption<number>(none).getOrElse(v)
+    return Promise.all([te1.run(), te2.run()]).then(([b1, b2]) => {
+      expect(b1).toEqual(1)
+      expect(b2).toEqual(42)
+    })
+  })
+
+  it('fromTask', () => {
+    const to1 = fromTask(new Task(() => Promise.resolve(none)))
+    return Promise.all([to1.run, taskOptionNone.run]).then(([b1, b2]) => {
+      expect(b1).toEqual(b2)
+    })
+  })
+
+  it('tryCatch', () => {
+    const to1 = tryCatch(() => Promise.reject())
+    return to1.run().then(b1 => expect(b1).toEqual(none))
+  })
+
   describe('withTimeout', () => {
     it('should return successfully within the timeout', () => {
       const t = new TaskOption(delay(100, some('value'))).withTimeout(some('fallback'), 500)
--- test/TaskValidation.ts
+++ test/TaskValidation.ts
@@ -1,8 +1,32 @@
-import { TaskValidation } from '../src/TaskValidation'
-import { delay } from 'fp-ts/lib/Task'
+import { TaskValidation, taskValidation, getApplicative } from '../src/TaskValidation'
+import { delay, Task } from 'fp-ts/lib/Task'
 import { success, failure } from 'fp-ts/lib/Validation'
+import { getSemigroup, NonEmptyArray, make } from 'fp-ts/lib/NonEmptyArray2v'
 
 describe('TaskValidation', () => {
+  const taskValidationNelApplicative = getApplicative(getSemigroup<string>())
+
+  it('map', () => {
+    const double = (n: number): number => n * 2
+    return taskValidation
+      .map(taskValidationNelApplicative.of(1), double)
+      .value.run()
+      .then(e => expect(e).toEqual(success(2)))
+  })
+
+  it('fold', () => {
+    const f = (s: NonEmptyArray<string>): boolean => s.length < 2
+    const g = (n: number): boolean => n > 2
+    const te1 = taskValidationNelApplicative.of<number>(1).fold(f, g)
+    const te2 = new TaskValidation<NonEmptyArray<string>, number>(
+      new Task(() => Promise.resolve(failure(make<string>('failure', []))))
+    ).fold(f, g)
+    return Promise.all([te1.run(), te2.run()]).then(([b1, b2]) => {
+      expect(b1).toEqual(false)
+      expect(b2).toEqual(true)
+    })
+  })
+
   describe('withTimeout', () => {
     it('should return successfully within the timeout', () => {
       const t = new TaskValidation(delay(100, success('value'))).withTimeout(failure('fallback'), 500)
@@ -12,6 +36,20 @@ describe('TaskValidation', () => {
     it('should return the fallback value on timeout', () => {
       const t = new TaskValidation(delay(500, success('value'))).withTimeout(failure('fallback'), 100)
       return t.value.run().then(v => expect(v).toEqual(failure('fallback')))
+    })
+  })
+
+  describe('getApplicative', () => {
+    it('ap', () => {
+      const double = (n: number): number => n * 2
+      const fa = taskValidationNelApplicative.of(1)
+      const fab = taskValidationNelApplicative.of(double)
+      return taskValidationNelApplicative
+        .ap(fab, fa)
+        .value.run()
+        .then(e1 => {
+          expect(e1).toEqual(success(2))
+        })
     })
   })
 })
