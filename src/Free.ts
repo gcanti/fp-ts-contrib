73e92139386d847bc8c8b30352812b85accae765
Yuriy Bogomolov <cronus.cpp@gmail.com>
2019-07-02 12:15:41 +0300

    Add Free monad
    Add docs, tests & expression tree example

---
+++ src/Free.ts
@@ -0,0 +1,178 @@
+import { HKT, Kind, Kind2, Kind3, URIS, URIS2, URIS3 } from 'fp-ts/lib/HKT'
+import { Monad, Monad1, Monad2, Monad2C, Monad3 } from 'fp-ts/lib/Monad'
+import { pipeable } from 'fp-ts/lib/pipeable'
+
+/**
+ * @since 0.1.2
+ */
+export const URI = 'Free'
+
+/**
+ * @since 0.1.2
+ */
+export type URI = typeof URI
+
+declare module 'fp-ts/lib/HKT' {
+  interface URItoKind2<E, A> {
+    Free: Free<E, A>
+  }
+}
+
+/**
+ * @data
+ * @constructor Pure
+ * @constructor Impure
+ * @since 0.1.2
+ */
+export type Free<F, A> = Pure<F, A> | Impure<F, A, any>
+
+interface Pure<_F, A> {
+  readonly _tag: 'Pure'
+  readonly value: A
+}
+
+interface Impure<F, A, X> {
+  readonly _tag: 'Impure'
+  readonly fx: HKT<F, X>
+  readonly f: (x: X) => Free<F, A>
+}
+
+const impure = <F, A, X>(fx: HKT<F, X>, f: (x: X) => Free<F, A>): Free<F, A> => ({ _tag: 'Impure', fx, f })
+
+/**
+ * @since 0.1.2
+ */
+export const isPure = <F, A>(fa: Free<F, A>): fa is Pure<F, A> => fa._tag === 'Pure'
+
+/**
+ * @since 0.1.2
+ */
+export const isImpure = <F, A>(fa: Free<F, A>): fa is Impure<F, A, any> => fa._tag === 'Impure'
+
+/**
+ * Lift an impure value described by the generating type constructor `F` into the free monad
+ *
+ * @since 0.1.2
+ */
+export const liftF = <F, A>(fa: HKT<F, A>): Free<F, A> => impure(fa, a => free.of(a))
+
+const substFree = <F, G>(f: <A>(fa: HKT<F, A>) => Free<G, A>): (<A>(fa: Free<F, A>) => Free<G, A>) => {
+  function go<A>(fa: Free<F, A>): Free<G, A> {
+    switch (fa._tag) {
+      case 'Pure':
+        return free.of(fa.value)
+      case 'Impure':
+        return free.chain(f(fa.fx), x => go(fa.f(x)))
+    }
+  }
+  return go
+}
+
+/**
+ * Use a natural transformation to change the generating type constructor of a free monad
+ *
+ * @since 0.1.2
+ */
+export function hoistFree<F extends URIS3 = never, G extends URIS3 = never>(
+  nt: <U, L, A>(fa: Kind3<F, U, L, A>) => Kind3<G, U, L, A>
+): <A>(fa: Free<F, A>) => Free<G, A>
+export function hoistFree<F extends URIS2 = never, G extends URIS2 = never>(
+  nt: <L, A>(fa: Kind2<F, L, A>) => Kind2<G, L, A>
+): <A>(fa: Free<F, A>) => Free<G, A>
+export function hoistFree<F extends URIS = never, G extends URIS = never>(
+  nt: <A>(fa: Kind<F, A>) => Kind<G, A>
+): <A>(fa: Free<F, A>) => Free<G, A>
+export function hoistFree<F, G>(nt: <A>(fa: HKT<F, A>) => HKT<G, A>): <A>(fa: Free<F, A>) => Free<G, A>
+export function hoistFree<F, G>(nt: <A>(fa: HKT<F, A>) => HKT<G, A>): <A>(fa: Free<F, A>) => Free<G, A> {
+  return substFree(fa => liftF(nt(fa)))
+}
+
+/**
+ * @since 0.1.2
+ */
+export interface FoldFree3<M extends URIS3> {
+  <F extends URIS3, U, L, A>(nt: <X>(fa: Kind3<F, U, L, X>) => Kind3<M, U, L, X>, fa: Free<F, A>): Kind3<M, U, L, A>
+  <F extends URIS2, U, L, A>(nt: <X>(fa: Kind2<F, L, X>) => Kind3<M, U, L, X>, fa: Free<F, A>): Kind3<M, U, L, A>
+  <F extends URIS, U, L, A>(nt: <X>(fa: Kind<F, X>) => Kind3<M, U, L, X>, fa: Free<F, A>): Kind3<M, U, L, A>
+}
+
+/**
+ * @since 0.1.2
+ */
+export interface FoldFree2<M extends URIS2> {
+  <F extends URIS2, L, A>(nt: <X>(fa: Kind2<F, L, X>) => Kind2<M, L, X>, fa: Free<F, A>): Kind2<M, L, A>
+  <F extends URIS, L, A>(nt: <X>(fa: Kind<F, X>) => Kind2<M, L, X>, fa: Free<F, A>): Kind2<M, L, A>
+}
+
+/**
+ * @since 0.1.2
+ */
+export interface FoldFree2C<M extends URIS2, L> {
+  <F extends URIS2, A>(nt: <X>(fa: Kind2<F, L, X>) => Kind2<M, L, X>, fa: Free<F, A>): Kind2<M, L, A>
+  <F extends URIS, A>(nt: <X>(fa: Kind<F, X>) => Kind2<M, L, X>, fa: Free<F, A>): Kind2<M, L, A>
+}
+
+/**
+ * @since 0.1.2
+ */
+export function foldFree<M extends URIS3>(M: Monad3<M>): FoldFree3<M>
+export function foldFree<M extends URIS2>(M: Monad2<M>): FoldFree2<M>
+export function foldFree<M extends URIS2, L>(M: Monad2C<M, L>): FoldFree2C<M, L>
+export function foldFree<M extends URIS>(
+  M: Monad1<M>
+): <F extends URIS, A>(nt: <X>(fa: Kind<F, X>) => Kind<M, X>, fa: Free<F, A>) => Kind<M, A>
+export function foldFree<M>(M: Monad<M>): <F, A>(nt: <X>(fa: HKT<F, X>) => HKT<M, X>, fa: Free<F, A>) => HKT<M, A>
+export function foldFree<M>(M: Monad<M>): <F, A>(nt: any, fa: Free<F, A>) => HKT<M, A> {
+  return (nt, fa) => {
+    if (isPure(fa)) {
+      return M.of(fa.value)
+    } else {
+      return M.chain(nt(fa.fx), x => foldFree(M)(nt, fa.f(x)))
+    }
+  }
+}
+
+/**
+ * @since 0.1.2
+ */
+export const free: Monad2<URI> = {
+  URI,
+  /**
+   * @since 0.1.2
+   */
+  of: <F, A>(value: A): Free<F, A> => ({ _tag: 'Pure', value }),
+  /**
+   * @since 0.1.2
+   */
+  chain: <F, A, B>(ma: Free<F, A>, f: (a: A) => Free<F, B>): Free<F, B> =>
+    isPure(ma) ? f(ma.value) : impure(ma.fx, x => free.chain(ma.f(x), f)),
+  /**
+   * @since 0.1.2
+   */
+  map: (fa, f) => (isPure(fa) ? free.of(f(fa.value)) : impure(fa.fx, x => free.map(fa.f(x), f))),
+  /**
+   * @since 0.1.2
+   */
+  ap: (fab, fa) => free.chain(fab, f => free.map(fa, f))
+}
+
+const { ap, chain, map, flatten } = pipeable(free)
+
+export {
+  /**
+   * @since 0.1.2
+   */
+  ap,
+  /**
+   * @since 0.1.2
+   */
+  chain,
+  /**
+   * @since 0.1.2
+   */
+  map,
+  /**
+   * @since 0.1.2
+   */
+  flatten
+}
---
+++ test/Free.ts
@@ -0,0 +1,100 @@
+import * as assert from 'assert'
+import { identity, FunctionN } from 'fp-ts/lib/function'
+import { Semigroup, semigroupSum } from 'fp-ts/lib/Semigroup'
+import { Identity, identity as id, URI as IdURI } from 'fp-ts/lib/Identity'
+
+import { liftF, foldFree, free, hoistFree, isImpure } from '../src/Free'
+import { Do } from '../src/Do'
+
+const URI = 'Expr'
+type URI = typeof URI
+
+class Add<A> {
+  readonly _URI!: URI
+  readonly _A!: A
+  readonly _tag: 'Add' = 'Add'
+  constructor(readonly M: Semigroup<A>, readonly a: A, readonly b: A, readonly next: (result: A) => A) {}
+}
+class Lit<A> {
+  readonly _URI!: URI
+  readonly _A!: A
+  readonly _tag: 'Lit' = 'Lit'
+  constructor(readonly value: A, readonly next: (result: A) => A) {}
+}
+type ExprF<A> = Add<A> | Lit<A>
+
+declare module 'fp-ts/lib/HKT' {
+  interface URItoKind<A> {
+    Expr: ExprF<A>
+  }
+}
+
+describe('Free', () => {
+  describe('Monad properties', () => {
+    it('of', () => {
+      const fa = free.of<IdURI, number>(42)
+      assert.strictEqual(fa._tag, 'Pure')
+    })
+
+    it('ap', () => {
+      const fa = free.of<IdURI, number>(41)
+      const fab = free.of<IdURI, FunctionN<[number], number>>(x => x + 1)
+      const fb = free.ap(fab, fa)
+      assert.strictEqual(fb._tag, 'Pure')
+
+      const result = foldFree(id)(identity, fb)
+      assert.strictEqual(result, 42)
+    })
+
+    it('map', () => {
+      const fa = free.of<IdURI, number>(41)
+      const fb = free.map(fa, x => x + 1)
+      assert.strictEqual(fb._tag, 'Pure')
+
+      const result = foldFree(id)(identity, fb)
+      assert.strictEqual(result, 42)
+    })
+
+    it('chain', () => {
+      const fa = free.of<IdURI, number>(41)
+      const fb = free.chain(fa, x => free.of(x + 1))
+      assert.strictEqual(fb._tag, 'Pure')
+
+      const result = foldFree(id)(identity, fb)
+      assert.strictEqual(result, 42)
+    })
+  })
+
+  describe('Expression tree example', () => {
+    const add = <A>(M: Semigroup<A>) => (a: A, b: A) => liftF(new Add(M, a, b, identity))
+    const lit = <A>(a: A) => liftF(new Lit(a, identity))
+
+    const program = Do(free)
+      .bind('two', lit(2))
+      .bind('three', lit(3))
+      .bindL('sum', ({ two, three }) => add(semigroupSum)(two, three))
+      .return(({ sum }) => sum)
+
+    assert.strictEqual(isImpure(program), true)
+
+    const idEval = <A>(fa: ExprF<A>): Identity<A> => {
+      switch (fa._tag) {
+        case 'Lit':
+          return id.of(fa.next(fa.value))
+        case 'Add':
+          return id.of(fa.next(fa.M.concat(fa.a, fa.b)))
+      }
+    }
+
+    it('foldFree', () => {
+      const result = foldFree(id)(idEval, program)
+      assert.equal(result, 5)
+    })
+
+    it('hoistFree', () => {
+      const programId = hoistFree<URI, IdURI>(idEval)(program)
+      const result = foldFree(id)(identity, programId)
+      assert.equal(result, 5)
+    })
+  })
+})
