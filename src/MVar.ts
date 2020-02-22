0395dfce7f1cb123379e1a1dbcbaa5b55177a2b1
DenisFrezzato <frezzato.denis@gmail.com>
2020-02-22 22:06:14 +0100

    Add full example

--- src/MVar.ts
+++ src/MVar.ts
@@ -10,6 +10,109 @@ import * as A from 'fp-ts/lib/Array'
 import { constVoid } from 'fp-ts/lib/function'
 
 /**
+ * @example
+ * import * as IO from 'fp-ts/lib/IO'
+ * import * as MVar from 'fp-ts-contrib/src/MVar'
+ * import * as T from 'fp-ts/lib/Task'
+ * import { Do } from 'fp-ts-contrib/src/Do'
+ * import { log } from 'fp-ts/lib/Console'
+ * import { pipe } from 'fp-ts/lib/pipeable'
+ * import { randomRange } from 'fp-ts/lib/Random'
+ * import { sequenceT } from 'fp-ts/lib/Apply'
+ *
+ * interface Response {
+ *   status: 200 | 401
+ * }
+ *
+ * type Token = string
+ *
+ * const issuedTokens: Set<Token> = new Set()
+ * const isTokenValid = (token: Token): IO.IO<boolean> => () => issuedTokens.has(token)
+ * const addToken = (token: Token): IO.IO<void> => () => issuedTokens.add(token)
+ * const wipeTokens: IO.IO<void> = () => issuedTokens.clear()
+ *
+ * setInterval(
+ *   pipe(
+ *     wipeTokens,
+ *     IO.chain(() => log('Tokens wiped.'))
+ *   ),
+ *   500
+ * )
+ *
+ * const tokenVar = MVar.newEmptyMVar<Token>()
+ *
+ * const loginRequest: T.Task<Token> = pipe(
+ *   log('Logging in'),
+ *   IO.chain(() => randomRange(1, 9999)),
+ *   IO.map(n => `token-${Math.floor(n)}`),
+ *   IO.chainFirst(token => sequenceT(IO.io)(addToken(token), log(`New token "${token}" issued.`))),
+ *   T.fromIO,
+ *   T.delay(2)
+ * )
+ *
+ * const login: T.Task<void> = pipe(
+ *   loginRequest,
+ *   T.chain(newToken => MVar.put(newToken)(tokenVar))
+ * )
+ *
+ * const someRequest = (token: Token): T.Task<Response> =>
+ *   pipe(
+ *     randomRange(50, 200),
+ *     T.fromIO,
+ *     T.chain(wait =>
+ *       pipe(
+ *         T.fromIO(isTokenValid(token)),
+ *         T.map(isValid => ({ status: isValid ? (200 as const) : (401 as const) })),
+ *         T.delay(wait)
+ *       )
+ *     )
+ *   )
+ *
+ * const handleLogout: T.Task<void> = pipe(
+ *   // Take the token out of the MVar. Other concurrent requests
+ *   // have to wait for a new token. This prevents race conditions.
+ *   MVar.take(tokenVar),
+ *   T.chain(() => loginRequest),
+ *   T.chain(newToken => MVar.put(newToken)(tokenVar))
+ * )
+ *
+ * const runRequest = (request: (token: Token) => T.Task<Response>): T.Task<Response> =>
+ *   Do(T.task)
+ *     .bind('id', T.fromIO(pipe(randomRange(1000, 9999), IO.map(Math.floor))))
+ *     .bind('token', MVar.read(tokenVar))
+ *     .bindL('response', ({ id, token }) =>
+ *       pipe(
+ *         T.fromIO(log(`[${id}] Request with token ${token}.`)),
+ *         T.chain(() => request(token)),
+ *         T.chainFirst(_ => T.fromIO(log(`[${id}] ${_.status}`))),
+ *         T.chain(res =>
+ *           res.status === 401
+ *             ? pipe(
+ *                 T.fromIO(log(`[${id}] Session expired`)),
+ *                 T.chain(() => handleLogout),
+ *                 T.chain(() => runRequest(request))
+ *               )
+ *             : T.of(res)
+ *         )
+ *       )
+ *     )
+ *     .return(({ response }) => response)
+ *
+ * const halt: IO.IO<void> = () => process.exit(0)
+ *
+ * pipe(
+ *   login,
+ *   T.chain(() => runRequest(someRequest)),
+ *   T.chain(() => T.delay(5)(runRequest(someRequest))),
+ *   T.chain(() => T.delay(25)(runRequest(someRequest))),
+ *   T.chain(() => T.delay(200)(runRequest(someRequest))),
+ *   T.chain(() => T.delay(1)(runRequest(someRequest))),
+ *   T.chain(() => T.delay(1)(runRequest(someRequest))),
+ *   T.chain(() => T.delay(250)(runRequest(someRequest))),
+ *   T.chain(() => T.delay(1)(runRequest(someRequest))),
+ *   T.chain(() => T.fromIO(halt))
+ * )()
+ *
  * @since 0.1.13
  */
 class MVar<T> {
