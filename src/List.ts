a91e2268f2226b830a2173b7a2b1734e1d4d23cd
Maxwell Brown <maxwellbrown1990@gmail.com>
2020-09-17 15:44:38 -0400

    Add list enhancements

--- src/List.ts
+++ src/List.ts
@@ -3,16 +3,20 @@
  *
  * @since 0.1.8
  */
-import { Applicative } from 'fp-ts/lib/Applicative'
+import { Applicative as ApplicativeHKT, Applicative1 } from 'fp-ts/lib/Applicative'
+import { Apply1 } from 'fp-ts/lib/Apply'
 import * as A from 'fp-ts/lib/Array'
 import * as Eq from 'fp-ts/lib/Eq'
-import { Foldable1 } from 'fp-ts/lib/Foldable'
-import { Predicate, Refinement } from 'fp-ts/lib/function'
+import { Foldable1, intercalate } from 'fp-ts/lib/Foldable'
+import { flow, pipe, Predicate, Refinement } from 'fp-ts/lib/function'
 import { Functor1 } from 'fp-ts/lib/Functor'
 import { HKT } from 'fp-ts/lib/HKT'
+import { Monad1 } from 'fp-ts/lib/Monad'
+import { Monoid, monoidString } from 'fp-ts/lib/Monoid'
 import * as O from 'fp-ts/lib/Option'
 import { Traversable1 } from 'fp-ts/lib/Traversable'
-import { Monoid } from 'fp-ts/lib/Monoid'
+import { Semigroup } from 'fp-ts/lib/Semigroup'
+import { Show } from 'fp-ts/lib/Show'
 
 // -------------------------------------------------------------------------------------
 // model
@@ -265,28 +269,13 @@ export function dropLeftWhile<A>(predicate: Predicate<A>): (fa: List<A>) => List
 // non-pipeables
 // -------------------------------------------------------------------------------------
 
-const map_ = <A, B>(fa: List<A>, f: (a: A) => B) =>
-  A.array.reduceRight<A, List<B>>(toArray(fa), nil, (a, b) => cons(f(a), b))
-const reduce_: Foldable1<URI>['reduce'] = (fa, b, f) => {
-  let out = b
-  let l = fa
-  while (isCons(l)) {
-    out = f(out, l.head)
-    l = l.tail
-  }
-  return out
-}
-const reduceRight_: Foldable1<URI>['reduceRight'] = (fa, b, f) => A.array.reduceRight(toArray(fa), b, f)
-const foldMap_: Foldable1<URI>['foldMap'] = (M) => (fa, f) => {
-  let out = M.empty
-  let l = fa
-  while (isCons(l)) {
-    out = M.concat(out, f(l.head))
-    l = l.tail
-  }
-  return out
-}
-const traverse_ = <F>(F: Applicative<F>): (<A, B>(ta: List<A>, f: (a: A) => HKT<F, B>) => HKT<F, List<B>>) => {
+const map_: Functor1<URI>['map'] = (fa, f) => pipe(fa, map(f))
+const ap_: Apply1<URI>['ap'] = (fab, fa) => pipe(fab, ap(fa))
+const chain_: Monad1<URI>['chain'] = (ma, f) => pipe(ma, chain(f))
+const reduce_: Foldable1<URI>['reduce'] = (fa, b, f) => pipe(fa, reduce(b, f))
+const reduceRight_: Foldable1<URI>['reduceRight'] = (fa, b, f) => pipe(fa, reduceRight(b, f))
+const foldMap_: Foldable1<URI>['foldMap'] = (M) => (fa, f) => pipe(fa, foldMap(M)(f))
+const traverse_ = <F>(F: ApplicativeHKT<F>): (<A, B>(ta: List<A>, f: (a: A) => HKT<F, B>) => HKT<F, List<B>>) => {
   return <A, B>(ta: List<A>, f: (a: A) => HKT<F, B>) =>
     list.reduceRight(ta, F.of<List<B>>(nil), (a, fbs) =>
       F.ap(
@@ -295,7 +284,7 @@ const traverse_ = <F>(F: Applicative<F>): (<A, B>(ta: List<A>, f: (a: A) => HKT<
       )
     )
 }
-const sequence_ = <F>(F: Applicative<F>) => <A>(ta: List<HKT<F, A>>): HKT<F, List<A>> => {
+const sequence_ = <F>(F: ApplicativeHKT<F>) => <A>(ta: List<HKT<F, A>>): HKT<F, List<A>> => {
   return list.reduceRight(ta, F.of<List<A>>(nil), (a, fas) =>
     F.ap(
       F.map(fas, (as) => (a: A) => cons(a, as)),
@@ -312,27 +301,106 @@ const sequence_ = <F>(F: Applicative<F>) => <A>(ta: List<HKT<F, A>>): HKT<F, Lis
  * @category Functor
  * @since 0.1.18
  */
-export const map: <A, B>(f: (a: A) => B) => (fa: List<A>) => List<B> = (f) => (fa) => map_(fa, f)
+export const map = <A, B>(f: (a: A) => B) => (fa: List<A>): List<B> =>
+  pipe(
+    toArray(fa),
+    A.reduceRight<A, List<B>>(nil, (a, b) => cons(f(a), b))
+  )
+
+/**
+ * @category Functor
+ * @since 0.1.20
+ */
+export const ap: <A>(fa: List<A>) => <B>(fab: List<(a: A) => B>) => List<B> = (fa) => chain((f) => pipe(fa, map(f)))
+
+/**
+ * @category Apply
+ * @since 0.1.20
+ */
+export const apFirst: <B>(fb: List<B>) => <A>(fa: List<A>) => List<A> = (fb) =>
+  flow(
+    map((a) => () => a),
+    ap(fb)
+  )
+
+/**
+ * @category Apply
+ * @since 0.1.20
+ */
+export const apSecond = <B>(fb: List<B>): (<A>(fa: List<A>) => List<B>) =>
+  flow(
+    map(() => (b: B) => b),
+    ap(fb)
+  )
+
+/**
+ * @category Monad
+ * @since 0.1.20
+ */
+export const chain = <A, B>(f: (a: A) => List<B>) => (ma: List<A>): List<B> =>
+  pipe(
+    ma,
+    foldLeft(
+      () => nil,
+      (x, xs) => {
+        const S = getSemigroup<B>()
+        let out = f(x)
+        let l = xs
+        while (isCons(l)) {
+          out = S.concat(out, f(l.head))
+          l = l.tail
+        }
+        return out
+      }
+    )
+  )
+
+/**
+ * @category Monad
+ * @since 0.1.20
+ */
+export const chainFirst: <A, B>(f: (a: A) => List<B>) => (fa: List<A>) => List<A> = (f) =>
+  chain((a) =>
+    pipe(
+      f(a),
+      map(() => a)
+    )
+  )
 
 /**
  * @category Foldable
  * @since 0.1.18
  */
-export const reduce: <A, B>(b: B, f: (b: B, a: A) => B) => (fa: List<A>) => B = (b, f) => (fa) => reduce_(fa, b, f)
+export const reduce: <A, B>(b: B, f: (b: B, a: A) => B) => (fa: List<A>) => B = (b, f) => (fa) => {
+  let out = b
+  let l = fa
+  while (isCons(l)) {
+    out = f(out, l.head)
+    l = l.tail
+  }
+  return out
+}
 
 /**
  * @category Foldable
  * @since 0.1.18
  */
 export const reduceRight: <A, B>(b: B, f: (a: A, b: B) => B) => (fa: List<A>) => B = (b, f) => (fa) =>
-  reduceRight_(fa, b, f)
+  pipe(toArray(fa), A.reduceRight(b, f))
 
 /**
  * @category Foldable
  * @since 0.1.18
  */
-export const foldMap: <M>(M: Monoid<M>) => <A>(f: (a: A) => M) => (fa: List<A>) => M = (M) => (f) => (fa) =>
-  foldMap_(M)(fa, f)
+export const foldMap: <M>(M: Monoid<M>) => <A>(f: (a: A) => M) => (fa: List<A>) => M = (M) => (f) => (fa) => {
+  let out = M.empty
+  let l = fa
+  while (isCons(l)) {
+    out = M.concat(out, f(l.head))
+    l = l.tail
+  }
+  return out
+}
 
 // TODO: add pipeable traverse when fp-ts version >= 2.6.3
 // /**
@@ -351,7 +419,7 @@ export const foldMap: <M>(M: Monoid<M>) => <A>(f: (a: A) => M) => (fa: List<A>) 
  * @since 0.1.18
  */
 export const sequence: Traversable1<URI>['sequence'] = <F>(
-  F: Applicative<F>
+  F: ApplicativeHKT<F>
 ): (<A>(ta: List<HKT<F, A>>) => HKT<F, List<A>>) => sequence_(F)
 
 /**
@@ -452,11 +520,77 @@ export const getEq = <A>(E: Eq.Eq<A>): Eq.Eq<List<A>> => ({
 
 /**
  * @category instances
+ * @since 0.1.20
+ */
+export const getShow = <A>(S: Show<A>): Show<List<A>> => ({
+  show: (as) =>
+    pipe(
+      as,
+      map(S.show),
+      foldLeft(
+        () => 'Nil',
+        (x, xs) => `(${intercalate(monoidString, Foldable)(' : ', cons(x, xs))} : Nil)`
+      )
+    )
+})
+
+/**
+ * @category instances
+ * @since 0.1.20
+ */
+export const getSemigroup = <A>(): Semigroup<List<A>> => ({
+  concat: (xs, ys) => pipe(xs, reduceRight(ys, cons))
+})
+
+/**
+ * @category instances
+ * @since 0.1.20
+ */
+export const getMonoid = <A>(): Monoid<List<A>> => ({
+  ...getSemigroup(),
+  empty: nil
+})
+
+/**
+ * @category instances
  * @since 0.1.18
  */
 export const Functor: Functor1<URI> = {
   URI,
   map: map_
+}
+
+/**
+ * @category instances
+ * @since 0.1.20
+ */
+export const Apply: Apply1<URI> = {
+  URI,
+  map: map_,
+  ap: ap_
+}
+
+/**
+ * @category instances
+ * @since 0.1.20
+ */
+export const Applicative: Applicative1<URI> = {
+  URI,
+  map: map_,
+  ap: ap_,
+  of
+}
+
+/**
+ * @category instances
+ * @since 0.1.20
+ */
+export const Monad: Monad1<URI> = {
+  URI,
+  map: map_,
+  ap: ap_,
+  of,
+  chain: chain_
 }
 
 /**
@@ -497,6 +631,60 @@ export const list: Functor1<URI> & Foldable1<URI> & Traversable1<URI> = {
   traverse: traverse_,
   sequence: sequence_
 }
+
+// -------------------------------------------------------------------------------------
+// do notation
+// -------------------------------------------------------------------------------------
+
+/**
+ * @internal
+ */
+const bind_ = <A, N extends string, B>(
+  a: A,
+  name: Exclude<N, keyof A>,
+  b: B
+): { [K in keyof A | N]: K extends keyof A ? A[K] : B } => Object.assign({}, a, { [name]: b }) as any
+
+/**
+ * @since 0.1.20
+ */
+export const bindTo = <N extends string>(name: N) => <A>(fa: List<A>): List<{ [K in N]: A }> =>
+  pipe(
+    fa,
+    map((a) => bind_({}, name, a))
+  )
+
+/**
+ * @since 0.1.20
+ */
+export const bind = <N extends string, A, B>(name: Exclude<N, keyof A>, f: (a: A) => List<B>) => (
+  fa: List<A>
+): List<{ [K in keyof A | N]: K extends keyof A ? A[K] : B }> =>
+  pipe(
+    fa,
+    chain((a) =>
+      pipe(
+        f(a),
+        map((b) => bind_(a, name, b))
+      )
+    )
+  )
+
+// -------------------------------------------------------------------------------------
+// pipeable sequence S
+// -------------------------------------------------------------------------------------
+
+/**
+ * @since 0.1.20
+ */
+export const apS = <A, N extends string, B>(
+  name: Exclude<N, keyof A>,
+  fb: List<B>
+): ((fa: List<A>) => List<{ [K in keyof A | N]: K extends keyof A ? A[K] : B }>) =>
+  flow(
+    map((a) => (b: B) => bind_(a, name, b)),
+    ap(fb)
+  )
 
 // -------------------------------------------------------------------------------------
 // utils
