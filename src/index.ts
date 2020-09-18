8da807d7f05445fc740b98d0e10ca146b805bcbe
Wayne Van Son <waynevanson@gmail.com>
2020-09-18 22:40:49 +0000

    feat(*): export modules from main entry point

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,66 @@
+//
+// Modules
+//
 /**
- * @since 0.1.0
+ * @since 0.1.20
+ */
+export * as align from './Align'
+/**
+ * @since 0.1.20
+ */
+export * as alignArray from './Align/Array'
+/**
+ * @since 0.1.20
+ */
+export * as arrayOption from './ArrayOption'
+/**
+ * @since 0.1.20
+ */
+export * as alignRecord from './Align/Record'
+/**
+ * @since 0.1.20
+ */
+export * as semialign from './Semialign'
+/**
+ * @since 0.1.20
+ */
+export * as semialignNonEmptyArray from './Semialign/NonEmptyArray'
+/**
+ * @since 0.1.20
+ */
+export * as free from './Free'
+/**
+ * @since 0.1.20
+ */
+export * as iOOption from './IOOption'
+/**
+ * @since 0.1.20
+ */
+export * as list from './List'
+/**
+ * @since 0.1.20
+ */
+export * as readerIO from './ReaderIO'
+/**
+ * @since 0.1.20
+ */
+export * as stateEither from './StateEither'
+/**
+ * @since 0.1.20
+ */
+export * as stateIO from './StateIO'
+/**
+ * @since 0.1.20
+ */
+export * as stateTaskEither from './StateTaskEither'
+/**
+ * @since 0.1.20
+ */
+export * as taskOption from './TaskOption'
+/**
+ * @since 0.1.20
+ */
+export * as zipper from './Zipper'
+
  */
 import {} from 'fp-ts'
