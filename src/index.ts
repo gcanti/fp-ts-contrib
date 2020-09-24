d61376f283583af9ca5110383aef75a562493f1b
Wayne Van Son <waynevanson@gmail.com>
2020-09-24 07:09:47 +0000

    fix(exports): index level exports not compatible

--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,112 @@
 /**
  * @since 0.1.0
  */
-//
-// Modules
-//
-/**
- * @since 0.1.20
- */
-export * as align from './Align'
-/**
- * @since 0.1.20
- */
-export * as alignArray from './Align/Array'
-/**
- * @since 0.1.20
- */
-export * as arrayOption from './ArrayOption'
-/**
- * @since 0.1.20
- */
-export * as alignRecord from './Align/Record'
-/**
- * @since 0.1.20
- */
-export * as semialign from './Semialign'
-/**
- * @since 0.1.20
- */
-export * as semialignNonEmptyArray from './Semialign/NonEmptyArray'
-/**
- * @since 0.1.20
- */
-export * as free from './Free'
-/**
- * @since 0.1.20
- */
-export * as iOOption from './IOOption'
-/**
- * @since 0.1.20
- */
-export * as list from './List'
-/**
- * @since 0.1.20
- */
-export * as readerIO from './ReaderIO'
-/**
- * @since 0.1.20
- */
-export * as stateEither from './StateEither'
-/**
- * @since 0.1.20
- */
-export * as stateIO from './StateIO'
-/**
- * @since 0.1.20
- */
-export * as stateTaskEither from './StateTaskEither'
-/**
- * @since 0.1.20
- */
-export * as taskOption from './TaskOption'
-/**
- * @since 0.1.20
- */
-export * as zipper from './Zipper'
 
-//
-// Helpers, combinators and utilites
-//
-/**
- * @since 0.1.20
- */
-export * as Do from './Do'
-/**
- * @since 0.1.20
- */
-export * as regExp from './RegExp'
-/**
- * @since 0.1.20
- */
-export * as batchTraverse from './batchTraverse'
-/**
- * @since 0.1.20
- */
-export * as collectUntil from './collectUntil'
-/**
- * @since 0.1.20
- */
-export * as filterA from './filterA'
-/**
- * @since 0.1.20
- */
-export * as time from './time'
+import * as align from './Align'
+import * as alignArray from './Align/Array'
+import * as arrayOption from './ArrayOption'
+import * as alignRecord from './Align/Record'
+import * as semialign from './Semialign'
+import * as semialignNonEmptyArray from './Semialign/NonEmptyArray'
+import * as free from './Free'
+import * as IOOption from './IOOption'
+import * as list from './List'
+import * as readerIO from './ReaderIO'
+import * as stateEither from './StateEither'
+import * as stateIO from './StateIO'
+import * as stateTaskEither from './StateTaskEither'
+import * as taskOption from './TaskOption'
+import * as zipper from './Zipper'
+import * as Do from './Do'
+import * as regExp from './RegExp'
+import * as batchTraverse from './batchTraverse'
+import * as collectUntil from './collectUntil'
+import * as filterA from './filterA'
+import * as time from './time'
+
+export {
+  /**
+   * @since 0.1.20
+   */
+  align,
+  /**
+   * @since 0.1.20
+   */
+  alignArray,
+  /**
+   * @since 0.1.20
+   */
+  arrayOption,
+  /**
+   * @since 0.1.20
+   */
+  alignRecord,
+  /**
+   * @since 0.1.20
+   */
+  semialign,
+  /**
+   * @since 0.1.20
+   */
+  semialignNonEmptyArray,
+  /**
+   * @since 0.1.20
+   */
+  free,
+  /**
+   * @since 0.1.20
+   */
+  IOOption,
+  /**
+   * @since 0.1.20
+   */
+  list,
+  /**
+   * @since 0.1.20
+   */
+  readerIO,
+  /**
+   * @since 0.1.20
+   */
+  stateEither,
+  /**
+   * @since 0.1.20
+   */
+  stateIO,
+  /**
+   * @since 0.1.20
+   */
+  stateTaskEither,
+  /**
+   * @since 0.1.20
+   */
+  taskOption,
+  /**
+   * @since 0.1.20
+   */
+  zipper,
+  /**
+   * @since 0.1.20
+   */
+  Do,
+  /**
+   * @since 0.1.20
+   */
+  regExp,
+  /**
+   * @since 0.1.20
+   */
+  batchTraverse,
+  /**
+   * @since 0.1.20
+   */
+  collectUntil,
+  /**
+   * @since 0.1.20
+   */
+  filterA,
+  /**
+   * @since 0.1.20
+   */
+  time
+}
